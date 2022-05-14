62a09fe336b5b6bbec2ade04f0aaaeee36c56c36
sebek78 <sebasporek@gmail.com>
2022-05-14 12:26:01 +0200

    Server: Add validtaion to CreateUserDto

--- apps/stock-api/src/users/dto/create-user.dto.ts
+++ apps/stock-api/src/users/dto/create-user.dto.ts
@@ -1,4 +1,23 @@
+import { MaxLength, MinLength } from 'class-validator';
+
 export class CreateUserDto {
+  @MinLength(4, {
+    // 'Username is too short. At least 4 characters.'
+    message: 'Nazwa użytkownika jest za krótka. Minimim 4 znaki.',
+  })
+  @MaxLength(32, {
+    // 'Username is too long. Maximum 32 characters.'
+    message: 'Nazwa uzytkownika jest za długa. Maksymalnie 32 znaki.',
+  })
   username: string;
+
+  @MinLength(8, {
+    // 'Password is too short. At least 8 characters.'
+    message: 'Hasło jest za krótkie. Minimim 8 znaków.',
+  })
+  @MaxLength(40, {
+    // 'Password is too long. Maximum 40 characters.'
+    message: 'Hasło jest za długie. Maksymalnie 40 znaków',
+  })
   password: string;
 }
